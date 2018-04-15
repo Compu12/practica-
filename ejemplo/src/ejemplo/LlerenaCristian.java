8b049959c8424b73b1f08ac2bf9b3380d3a9567d
Xavier Cañizares <Xavier@DESKTOP-FIHE4PC>
2018-04-15 15:58:52 -0500

    ....

--- ejemplo/src/ejemplo/LlerenaCristian.java
+++ ejemplo/src/ejemplo/LlerenaCristian.java
@@ -1,3 +1,6 @@
+package ejemplo;
+import javax.swing.JOptionPane;
+
 
 public class LlerenaCristian {
       public static void main(String[] args) {
--- ejemplo/src/ejemplo/XavierCañizares.java
+++ ejemplo/src/ejemplo/XavierCañizares.java
@@ -3,11 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package ejemplo;
 
 /**
  *
  * @author Xavier
  */
 public class XavierCañizares {
-    
+    //hola 
 }
