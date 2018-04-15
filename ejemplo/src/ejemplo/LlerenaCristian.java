53956f04a0536e9c5c289731fe830c3cb6ad0db5
CRISTIANVALENCIARFC <38392038+CRISTIANVALENCIARFC@users.noreply.github.com>
2018-04-15 15:52:11 -0500

    Create LlerenaCristian.java
    cristian llerena

---
+++ ejemplo/src/ejemplo/LlerenaCristian.java
@@ -0,0 +1,15 @@
+
+package ejemplo;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author CRISTIAN
+ */
+public class LlerenaCristian {
+      public static void main(String[] args) {
+        
+        JOptionPane.showMessageDialog(null, "Hola COMPAS");
+    }
+}
